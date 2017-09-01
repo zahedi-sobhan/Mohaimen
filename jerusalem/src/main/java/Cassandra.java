48c849c5014ed1cd9862f2ad56fd52aa251526ad
zahedi_sobhan <zahedi_sobhan@live.com>
2017-09-01 22:47:51 +0430

    initial commit

---
+++ jerusalem/src/main/java/Cassandra.java
@@ -0,0 +1,19 @@
+import com.datastax.driver.core.Session;
+
+/**
+ * Created by sobhan on 5/4/17.
+ */
+
+public class Cassandra {
+    // default port  : 9042
+    private Session session ;
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("invoke simple");
+
+    }
+}
