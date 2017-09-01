e7efa15c44baef5cb31d3036ae2309b38f9a0537
zahedi_sobhan <zahedi_sobhan@live.com>
2017-09-02 01:07:42 +0430

    Cassandra

--- jerusalem/src/main/java/Cassandra.java
+++ jerusalem/src/main/java/Cassandra.java
@@ -1,19 +1,35 @@
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
+import java.util.List;
 
 /**
  * Created by sobhan on 5/4/17.
  */
 
 public class Cassandra {
-    // default port  : 9042
-    private Session session ;
+    /**
+     *
+     * @param code that is database column
+     * @return
+     */
 
 
+    public List read (String code) {
+        //SQL query
+        String query = "SELECT * FROM emp Where id =" + code;
 
+        //Creating Cluster object and puss ip address to them
+        Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
 
+        //Creating Session object
+        Session session = cluster.connect("var");
 
-    public static void main(String[] args) {
-        System.out.println("invoke simple");
+        //Getting the ResultSet
+        ResultSet result = session.execute(query);
 
+        return result.all() ;
     }
+
+
 }
